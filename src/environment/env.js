0729d66a3a2e57f1704e993b85f37ff27e2f3b40
a52290451 <cuchitrin555@hotmail.com>
2025-02-27 21:52:53 -0500

    feat: actualziación de URL Back

--- src/environment/env.js
+++ src/environment/env.js
@@ -1,6 +1,6 @@
 const environment = {
-    backUrl: 'http://127.0.0.1:5000',
-    //backUrl: 'http://ec2-44-206-219-250.compute-1.amazonaws.com:5000',
+    //backUrl: 'http://127.0.0.1:5000',
+    backUrl: 'http://ec2-18-117-89-214.us-east-2.compute.amazonaws.com/:5000',
 };
 
 export default environment;
